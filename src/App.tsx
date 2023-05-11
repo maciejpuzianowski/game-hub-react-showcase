82e4575f1284a17d030dd600155dae31682a0568
Maciej Puzianowski <maciekpuzianowski@gmail.com>
2023-05-11 21:31:38 +0200

    spinner genres

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
+        <GridItem area="aside" padding={7}>
           <GenreList />
         </GridItem>
       </Show>
